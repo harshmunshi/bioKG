a2c2c8506d1ff08172caa0be07f2c8eb6070d872
harsh.munshi <harsh.munshi@motius.de>
2026-07-02 10:02:57 +0200

    fix: stale import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .entity_linker import EntityLinker
-from .evaluation import Evaluator, evaluate_projection_alignment
+from .evaluation import evaluate_projection_alignment
 from .model_profile import resolve_model_profile
 
-__all__ = ["EntityLinker", "Evaluator", "evaluate_projection_alignment", "resolve_model_profile"]
+__all__ = ["EntityLinker", "evaluate_projection_alignment", "resolve_model_profile"]
